9a6ffbd4e45e1d7944a49f34917f802b7d0bbbf7
mubarrah <bsef23m525@pucit.edu.pk>
2026-06-30 12:04:04 +0500

    Setup and Service Module

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,77 @@
+// angular import
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+// Project import
+import { AdminLayout } from './theme/layouts/admin-layout/admin-layout.component';
+import { GuestLayoutComponent } from './theme/layouts/guest-layout/guest-layout.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AdminLayout,
+    children: [
+      {
+        path: '',
+        redirectTo: '/dashboard/default',
+        pathMatch: 'full'
+      },
+      {
+        path: 'dashboard/default',
+        loadComponent: () => import('./demo/dashboard/default/default.component').then((c) => c.DefaultComponent)
+      },
+      {
+        path: 'typography',
+        loadComponent: () => import('./demo/component/basic-component/typography/typography.component').then((c) => c.TypographyComponent)
+      },
+      {
+        path: 'color',
+        loadComponent: () => import('./demo/component/basic-component/color/color.component').then((c) => c.ColorComponent)
+      },
+      {
+        path: 'sample-page',
+        loadComponent: () => import('./demo/others/sample-page/sample-page.component').then((c) => c.SamplePageComponent)
+      },
+     {
+  path: 'property/setup',
+  loadComponent: () =>
+    import('./demo/pages/setup/setup.component')
+      .then(m => m.SetupComponent),
+  data: { title: 'Property Setup' }
+},
+{
+    path: 'services',
+    loadComponent: () =>
+        import('./demo/pages/services/services.component')
+        .then(c => c.ServicesComponent)
+},
+{
+  path: 'blank',
+  loadComponent: () =>
+    import('./demo/pages/blank/blank.component')
+      .then(c => c.BlankComponent)
+}
+    
+    ]
+  },
+  {
+    path: '',
+    component: GuestLayoutComponent,
+    children: [
+      {
+        path: 'login',
+        loadComponent: () => import('./demo/pages/authentication/auth-login/auth-login.component').then((c) => c.AuthLoginComponent)
+      },
+      {
+        path: 'register',
+        loadComponent: () =>
+          import('./demo/pages/authentication/auth-register/auth-register.component').then((c) => c.AuthRegisterComponent)
+      },
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
